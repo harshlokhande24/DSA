6bfd172e85588512db8114837915f1009b8059ad
harshlokhande24 <105076775+harshlokhande24@users.noreply.github.com>
2023-06-13 20:09:49 +0530

    Update ExpressionTree.cpp

--- ExpressionTree.cpp
+++ ExpressionTree.cpp
@@ -2,178 +2,88 @@
 //Harshwardhan Lokhande
 
 #include <iostream>
-#include <stack>
-#include <string>
-
+#include <ctype.h>
+#include <string.h>
 using namespace std;
 
-struct TreeNode{
-    char value;
-    TreeNode* left;
-    TreeNode* right;
+class node {
+    
+    char data;
+    node *left, *right;
+    
+    public:
+        void operand(char );
+        void operators(char );
+        void push(node *);
+        node *pop();
+        void inorder(node *);
+    
 };
 
-TreeNode* newNode(char value) {
-    TreeNode* node = new TreeNode();
-    node->value = value;
-    node->left = node->right = nullptr;
-    return node;
+node *stack[20];
+int top = -1;
+node r;
+
+void node :: push(node *root) {
+    top++;
+    stack[top] = root;
 }
 
-TreeNode* constructPostfixTree(string postfix) {
-    stack<TreeNode*> stk;
-    for (int i = 0; i < postfix.size(); i++) {
-        char c = postfix[i];
-        if (isdigit(c)) {
-            TreeNode* node = newNode(c);
-            stk.push(node);
+node * node :: pop() {
+    return stack[top--];
+}
+
+void node :: operand(char s) {
+    
+    node *root;
+    root = new node;
+    root->data = s;
+    root->left = NULL;
+    root->right = NULL;
+    push(root);
+}
+
+void node :: operators(char s) {
+    node *root;
+    root = new node;
+    root->data = s;
+    root->right = pop();
+    root->left = pop();
+    push(root);
+}
+
+void  node :: inorder(node *root) {
+    if(root) {
+        inorder(root->left);
+        cout<<root->data<<" ";
+        inorder(root->right);
+    }
+}
+
+int main()
+{
+    node r;
+    int i=0;
+    char s[20];
+    cout<<"Enter postfix expression: ";
+    cin>>s;
+    
+    while(s[i] != '\0') {
+        
+        if(isalnum(s[i])) {
+            cout<<"Operand: "<<s[i]<<endl;
+            r.operand(s[i]);
         }
         else {
-            TreeNode* right = stk.top();
-            stk.pop();
-            TreeNode* left = stk.top();
-            stk.pop();
-            TreeNode* node = newNode(c);
-            node->left = left;
-            node->right = right;
-            stk.push(node);
+            
+            cout<<"Operator: "<<s[i]<<endl;
+            r.operators(s[i]);
+            
         }
+        i++;
     }
-    return stk.top();
-}
+    cout<<"Inorder expression: ";
+    r.inorder(stack[top]);
 
-TreeNode* constructPrefixTree(string prefix) {
-    stack<TreeNode*> stk;
-    for (int i = prefix.size() - 1; i >= 0; i--) {
-        char c = prefix[i];
-        if (isdigit(c)) {
-            TreeNode* node = newNode(c);
-            stk.push(node);
-        }
-        else {
-            TreeNode* left = stk.top();
-            stk.pop();
-            TreeNode* right = stk.top();
-            stk.pop();
-            TreeNode* node = newNode(c);
-            node->left = left;
-            node->right = right;
-            stk.push(node);
-        }
-    }
-    return stk.top();
-}
-
-void recursiveInorder(TreeNode* root) {
-    if (root == nullptr) return;
-        recursiveInorder(root->left);
-        cout << root->value << " ";
-        recursiveInorder(root->right);
-    }
-    
-void iterativeInorder(TreeNode* root) {
-    stack<TreeNode*> stk;
-    TreeNode* curr = root;
-    while (curr != nullptr || !stk.empty()) {
-        while (curr != nullptr) {
-            stk.push(curr);
-            curr = curr->left;
-        }
-        curr = stk.top();
-        stk.pop();
-        cout << curr->value << " ";
-        curr = curr->right;
-    }
-}
-
-void recursivePreorder(TreeNode* root) {
-    if (root == nullptr) return;
-        cout << root->value << " ";
-        recursivePreorder(root->left);
-        recursivePreorder(root->right);
-    }
-    
-void iterativePreorder(TreeNode* root) {
-    if (root == nullptr) return;
-        stack<TreeNode*> stk;
-        stk.push(root);
-    while (!stk.empty()) {
-        TreeNode* curr = stk.top();
-        stk.pop();
-        cout << curr->value << " ";
-        if (curr->right != nullptr) stk.push(curr->right);
-        if (curr->left != nullptr) stk.push(curr->left);
-    }
-}
-
-void recursivePostorder(TreeNode* root) {
-    if (root == nullptr) return;
-        recursivePostorder(root->left);
-        recursivePostorder(root->right);
-        cout << root->value << " ";
-}
-
-void iterativePostorder(TreeNode* root) {
-    if (root == nullptr) return;
-    stack<TreeNode*> stk1, stk2;
-    stk1.push(root);
-    while (!stk1.empty()) {
-        TreeNode* curr = stk1.top();
-        stk1.pop();
-        stk2.push(curr);
-        if (curr->left != nullptr)
-            stk1.push(curr->left);
-        if (curr->right != nullptr)
-            stk1.push(curr->right);
-    }
-    while (!stk2.empty()) {
-        TreeNode* curr = stk2.top();
-        stk2.pop();
-        cout << curr->value << " ";
-    }
-}
-
-int main() {
-    int choice;
-    TreeNode* root;
-    while(choice!=3){
-        cout<<endl;
-        cout << "1. Construct expression tree from postfix expression\n";
-        cout << "2. Construct expression tree from prefix expression\n";
-        cout << "Enter your choice: ";
-        cin >> choice;
-        string expression;
-        cout << "Enter the expression: ";
-        cin >> expression;
-        if (choice == 1) {
-            root = constructPostfixTree(expression);
-        }
-        else{
-            root = constructPrefixTree(expression);
-        }
-        cout << "Recursive In-order traversal: ";
-        recursiveInorder(root);
-        cout << endl;
-        
-        cout << "Non-recursive In-order traversal: ";
-        iterativeInorder(root);
-        cout << endl;
-        
-        cout << "Recursive Pre-order traversal: ";
-        recursivePreorder(root);
-        cout << endl;
-        
-        cout << "Non-recursive Pre-order traversal: ";
-        iterativePreorder(root);
-        cout << endl;
-        
-        cout << "Recursive Post-order traversal: ";
-        recursivePostorder(root);
-        cout << endl;
-
-        cout << "Non-recursive Post-order traversal: ";
-        iterativePostorder(root);
-        cout << endl;
-    }
     return 0;
 }
